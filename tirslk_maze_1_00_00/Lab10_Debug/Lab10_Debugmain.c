1f6d5484c15f946d67baf8eae236dbbf230238bc
NECKTER <nick.scheele@yahoo.com>
2019-12-04 11:01:40 -0800

    worked once
    yes

--- tirslk_maze_1_00_00/Lab10_Debug/Lab10_Debugmain.c
+++ tirslk_maze_1_00_00/Lab10_Debug/Lab10_Debugmain.c
@@ -75,7 +75,7 @@ policies, either expressed or implied, of the FreeBSD Project.
 #include "..\inc\TExaS.h"
 
 int interrupt_count = 1;
-#define SIZE 5
+#define SIZE 3
 #define readFrequenct 10 //1 over this value is duty cycle
 #define ROM_start 0x00020000
 #define ROM_end 0x0003FFFF
@@ -89,6 +89,8 @@ int max = 2000;
 int adjust = 1000;
 int turn = 500;
 int wayOff = 0;
+int count = 0;
+int treasureFound = 0;
 
 void TimedPause(uint32_t time){
   Motor_Stop();
@@ -111,9 +113,11 @@ void collision(){
     if(Bump_Read() < 0x3F){
         Motor_Stop();
         Motor_Backward(max,max);
-        Clock_Delay1ms(1000);
+        Clock_Delay1ms(1500);
         Motor_Right(max,max);
-        Clock_Delay1ms(500);
+        Clock_Delay1ms(750);
+        Motor_Forward(max,max);
+        Clock_Delay1ms(750);
         Motor_Stop();
     }
 }
@@ -135,6 +139,26 @@ int32_t average(int32_t *values, uint8_t size){
     return average/size;
 }
 
+void right(int speed, int inplace){
+    REDLED &= ~0x01;
+    BLUELED |= 0x01;
+    if(!inplace) Motor_Forward(max,max-speed);
+    else Motor_Left(speed, speed);
+    count = 0;
+
+//    Clock_Delay1ms(50);
+}
+
+void left(int speed, int inplace){
+    BLUELED &= ~0x01;
+    REDLED |= 0x01;
+    if(!inplace)Motor_Forward(max-speed,max);
+    else Motor_Right(speed, speed);
+    count = 0;
+
+//    Clock_Delay1ms(100);
+}
+
 int main(void){
   // write this as part of Lab 10
     Clock_Init48MHz();
@@ -143,13 +167,16 @@ int main(void){
     Bump_Init();
     Motor_Init();
     TimerA1_Init(&collision,5000);
-    SysTick_Init((48*1000)*4, 0);
+    SysTick_Init((48*1000)*3, 0);
     EnableInterrupts();
     bufferInit();
     TimedPause(500);
-    TExaS_Init(LOGICANALYZER_P7);
+//    TExaS_Init(LOGICANALYZER_P7);
   while(1){
   // write this as part of Lab 10
+//      Motor_Stop();
+//      Clock_Delay1ms(100);
+
       IRavg = average(IRData, SIZE);
       switch (IRavg) {
         case 371://treasure
@@ -157,127 +184,56 @@ int main(void){
             BLUELED |= 0x01;
 //            Motor_Right(max,max);
 //            return 0;
+            treasureFound = treasureFound + 1;
+            TimedPause(500);
             break;
         case -370 ... -238://Way left
-            BLUELED &= ~0x01;
-            REDLED |= 0x01;
-//            Motor_Forward(wayOff,max);
-            Motor_Right(max, max);
+            left(4000, 1);
             break;
         case -237 ... -143://left
-            BLUELED &= ~0x01;
-            REDLED |= 0x01;
-            Motor_Forward(turn,max);
-//            Motor_Left(adjust, adjust);
+            left(max, 0);
             break;
         case -142 ... -71://slightly left
-            BLUELED &= ~0x01;
-            REDLED |= 0x01;
-            Motor_Forward(adjust,max);
-//            Motor_Left(turn, turn);
+            left(adjust, 0);
             break;
         case -70 ... 70://centered
             REDLED |= 0x01;
             BLUELED |= 0x01;
             Motor_Forward(max,max);
+            Clock_Delay1ms(50);
             break;
         case 71 ... 142://slightly right
-            REDLED &= ~0x01;
-            BLUELED |= 0x01;
-            Motor_Forward(max,adjust);
-//            Motor_Right(turn, turn);
+            right(adjust, 0);
             break;
         case 143 ... 237://right
-            REDLED &= ~0x01;
-            BLUELED |= 0x01;
-            Motor_Forward(max,turn);
-//            Motor_Right(adjust, adjust);
+            right(max, 0);
             break;
         case 238 ... 370://way right
-            REDLED &= ~0x01;
-            BLUELED |= 0x01;
-//            Motor_Forward(max,wayOff);
-            Motor_Left(max, max);
+            right(4000, 1);
             break;
-        default://spin??!!
+        default://spin??!!Lost
 //          REDLED &= ~0x01;
-            BLUELED &= ~0x01;
-            Motor_Backward(max,max);
-            Clock_Delay1ms(100);
-//            if(average(previous, SIZE)>0){
-//                Motor_Left(max,max);
-//            }else{
-//                Motor_Right(max,max);
-//            }
-//            Clock_Delay1ms(250);
+                BLUELED &= ~0x01;
+                Motor_Backward(max/2,max/2);
+                Clock_Delay1ms(10);
+                count = count + 1;
+            if(count == 19){
+                BLUELED |= ~0x01;
+                Motor_Backward(max,max);
+                Clock_Delay1ms(850);
+                Motor_Left(max, max);
+                Clock_Delay1ms(1000);
+                count = 0;
+/*                if(count2 == 1){
+                    Motor_Left(-max, max);
+                    Clock_Delay1ms(10000);
+                    break;
+                }*/
+//                count2 = 1;
+            }
             break;
           }//if backs up a few times in a row go back and turn left
       previous[j] = IRavg;
       j = (j+1)%SIZE;
   }
 }
-/*
-switch (IRavg) {
-    case -400://err Lost
-        REDLED &= ~0x01;
-        BLUELED &= ~0x01;
-        Motor_Backward(turn,turn);
-        Clock_Delay1ms(750);
-        if(average(previous, SIZE)>0){
-            Motor_Backward(wayOff,adjust);
-        }else{
-            Motor_Backward(adjust,wayOff);
-        }
-        Clock_Delay1ms(250);
-        break;
-    case 400://treasure
-        REDLED |= 0x01;
-        BLUELED |= 0x01;
-        Motor_Stop();
-//            return 0;
-        break;
-    case -360 ... -238://Way left
-        BLUELED &= ~0x01;
-        REDLED |= 0x01;
-        Motor_Forward(wayOff,max);
-        break;
-    case -237 ... -143://left
-        BLUELED &= ~0x01;
-        REDLED |= 0x01;
-        Motor_Forward(turn,max);
-        break;
-    case -142 ... -51://slightly left
-        BLUELED &= ~0x01;
-        REDLED |= 0x01;
-        Motor_Forward(adjust,max);
-        break;
-    case -50 ... 50://centered
-        REDLED |= 0x01;
-        BLUELED |= 0x01;
-        Motor_Forward(max,max);
-        break;
-    case 51 ... 142://slightly right
-        REDLED &= ~0x01;
-        BLUELED |= 0x01;
-        Motor_Forward(max,adjust);
-        break;
-    case 143 ... 237://right
-        REDLED &= ~0x01;
-        BLUELED |= 0x01;
-        Motor_Forward(max,turn);
-        break;
-    case 238 ... 360://way right
-        REDLED &= ~0x01;
-        BLUELED |= 0x01;
-        Motor_Forward(max,wayOff);
-        break;
-    default://spin??!!
-//            Motor_Forward(7500,0);
-        REDLED &= ~0x01;
-        BLUELED &= ~0x01;
-        Motor_Forward(2000,1700);
-//            Clock_Delay1ms(500);
-//            Motor_Stop();
-        break;
-  }
-      */
